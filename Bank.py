f21fae361b7d4ee9df8fa2ebc1bf15bb0dcd3217
Abdulla <abdulla18402@gmail.com>
2022-08-30 10:56:28 +0530

    first commit

---
+++ Bank.py
@@ -0,0 +1,19 @@
+ Account_number =int(input('Enter the Account number'))
+Cust_Name=input('Enter the Name')
+Branch_Name = input ('Enter the branch number')
+IFSC_Code=str('enter ifsc code')
+pan_number =str('enter the pan number')
+Address =input( 'enter the address')
+Phone_Number =int(input('enter the phone number'))
+Email =input('enter the email')
+deposit=int(input('enter the ammount'))
+min_balence=50000
+amount=100000
+widthdraw=1000
+deposit= min_balence+amount
+withdraw=amount- withdraw
+print('Account_number\n Cust_Name\n Branch_Code\n IFSC_Code\n pan_number\n Address\n Phone_Number\n Email\n min_balance\n amount\n widthdraw\n deposit\n widthdraw\n')
+print(Account_number,'\n',Cust_Name,'\n', Branch_Code,'\n', IFSC_Code,'\n', pan_number,'\n' ,Address,'\n' ,Phone_Number,'\n' ,Email,'\n',min_balance,'\n', amount,'\n' ,deposit,'\n' ,widthdraw,'\n')
+     
+      
+                  
