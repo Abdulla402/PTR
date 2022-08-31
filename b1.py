0721e1178595df08ef938f99213485ad896e64e1
Abdulla <abdulla18402@gmail.com>
2022-08-31 22:07:55 +0530

    first commit

---
+++ b1.py
@@ -0,0 +1,15 @@
+acc=int(input('enter the account number'))
+name=input('enter the name')
+branch=input('enter the branch')
+ifsc=str(input('enter the ifsc code'))
+phon=int(input('enter the number'))
+email=input('enter the mail')
+min_balance=1000
+amount=8000
+withdraw=100
+diposit=min_balance+amount
+withdraw=amount-withdraw
+withdraw=8000-1000
+print('acc\t name\t branch\t ifsc\t phon\t email\t min_balance\t amount\t amount\t diposit\t withdraw\t')
+print(acc,'\t', name,'\t', branch,'\t', ifsc,'\t', phon,'\t', email,'\t', min_balance,'\t', amount,'\t', amount,'\t' ,diposit,'\t', withdraw,'\t')
+
